465f9ada02d21c6e96a25b25f3919a7ba810fed4
Daniel Hinojosa <dhinojosa@evolutionnext.com>
2018-04-18 12:53:59 -0700

    Added new changes

--- src/main/java/com/xyzcorp/jersey/MyResource.java
+++ src/main/java/com/xyzcorp/jersey/MyResource.java
@@ -1,8 +1,5 @@
 package com.xyzcorp.jersey;
 
-import com.xyzcorp.hystrix.CommandHelloWorld;
-import com.xyzcorp.hystrix.Gate1Command;
-
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -23,9 +20,7 @@ public class MyResource {
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String getIt() {
-        Gate1Command gate1Command = new Gate1Command();
-        String result = gate1Command.execute();
-        return "Got it! Your name is " + result;
+        return "Got it! Welcome!";
     }
 }
 
